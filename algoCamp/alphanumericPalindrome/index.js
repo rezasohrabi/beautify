0f2a2fcf8094304a778d116255842c80d5bdc0c2
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-17 22:31:51 +0330

    chore: implementing alphanumeric palindrome using replace method and foor loop

--- algoCamp/alphanumericPalindrome/index.js
+++ algoCamp/alphanumericPalindrome/index.js
@@ -2,8 +2,23 @@ function palindrome(str) {
   if (str === null || str === undefined || typeof str !== 'string') {
     return false;
   }
-  const alphaNumericChars = str.toLowerCase().match(/[a-zA-Z0-9]/gi);
+  const alphaNumericChars = str.toLowerCase().match(/[a-zA-Z0-9]/g);
   return alphaNumericChars.join('') === alphaNumericChars.reverse().join('');
 }
 
-module.exports = palindrome;
+function palindrome2(str) {
+  if (str === null || str === undefined || typeof str !== 'string') {
+    return false;
+  }
+
+  const alphaNumericStr = str.toLowerCase().replace(/[\W_]/g, '');
+  const middleIndex = alphaNumericStr.length / 2;
+
+  for (let i = 0; i < middleIndex; i++)
+    if (alphaNumericStr[i] !== alphaNumericStr[alphaNumericStr.length - 1 - i])
+      return false;
+
+  return true;
+}
+
+module.exports = palindrome2;
