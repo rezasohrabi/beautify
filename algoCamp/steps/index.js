dc10e7edd495ee565ab06b8bc1c40d3a2a23b2b0
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-18 21:10:31 +0330

    chore: implement steps algo using recursion

--- algoCamp/steps/index.js
+++ algoCamp/steps/index.js
@@ -6,4 +6,37 @@ function steps(num) {
   }
 }
 
-module.exports = steps;
+function steps2(num) {
+  for (let row = 0; row < num; row++) {
+    let step = '';
+
+    for (let column = 0; column < num; column++) {
+      if (column <= row) {
+        step += '#';
+      } else {
+        step += ' ';
+      }
+    }
+    console.log(step);
+  }
+}
+
+function step3(num, row = 0, step = '') {
+  if (row === num) {
+    return;
+  }
+
+  if (step.length === num) {
+    console.log(step);
+    return step3(num, row + 1);
+  }
+
+  if (step.length <= row) {
+    step += '#';
+  } else {
+    step += ' ';
+  }
+  step3(num, row, step);
+}
+
+module.exports = step3;
