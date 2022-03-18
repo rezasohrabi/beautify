5fcd8d1021cc3b759aa7bd18aea3fb9e83c1d0b5
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-18 16:17:36 +0330

    chore: implement anagrams using two hashtable

--- algoCamp/anagrams/index.js
+++ algoCamp/anagrams/index.js
@@ -13,4 +13,27 @@ function anagrams(str1, str2) {
   );
 }
 
-module.exports = anagrams;
+function anagrams2(str1, str2) {
+  function hashtable(str) {
+    const hashtable = {};
+    for (let ch of str.replace(/[\W_]/g, '').toLowerCase()) {
+      hashtable[ch] = hashtable[ch] + 1 || 1;
+    }
+
+    return hashtable;
+  }
+  const hashtable1 = hashtable(str1);
+  const hashtable2 = hashtable(str2);
+
+  if (Object.keys(hashtable1).length !== Object.keys(hashtable2).length) {
+    return false;
+  }
+
+  for (let ch in hashtable1) {
+    if (hashtable1[ch] !== hashtable2[ch]) return false;
+  }
+
+  return true;
+}
+
+module.exports = anagrams2;
--- algoCamp/anagrams/index.test.js
+++ algoCamp/anagrams/index.test.js
@@ -4,10 +4,10 @@ test('anagrams("rail safety", "fairy tales") should return true', () => {
   expect(anagrams('_Rail safety!', 'fairy tales')).toEqual(true);
 });
 
-test('anagrams("rail safety", "fairy tales") should return true', () => {
+test('anagrams("_Rail safety", "fairy tales") should return true', () => {
   expect(anagrams('_Rail safety!', 'fairy tales')).toEqual(true);
 });
 
-test('anagrams("hi there", "bye there") should return true', () => {
+test('anagrams("hi there", "bye there") should return false', () => {
   expect(anagrams('hi there', 'bye there')).toEqual(false);
 });
