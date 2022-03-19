87a4111d8d1834650820d15a8d6dfbba03cedde0
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-19 09:22:54 +0330

    test: find vowels algo tests

---
+++ algoCamp/vowels/index.test.js
@@ -0,0 +1,17 @@
+const findVowels = require('./index');
+
+test('findVowels("this is your vowels") should return 6', () => {
+  expect(findVowels('this is your vowels')).toEqual(6);
+});
+
+test('findVowels("hi there!") should return 3', () => {
+  expect(findVowels('hi there!')).toEqual(3);
+});
+
+test('findVowels("Why do you ask?") should return 4', () => {
+  expect(findVowels('Why do you ask?')).toEqual(4);
+});
+
+test('findVowels("Why") should return 0', () => {
+  expect(findVowels('Why')).toEqual(0);
+});
