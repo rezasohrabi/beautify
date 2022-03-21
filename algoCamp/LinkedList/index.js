b7d415a37adf95b49370f30af4719087b0196b9f
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-21 20:51:10 +0330

    chore: implement size() method of linkedlist

--- algoCamp/LinkedList/index.js
+++ algoCamp/LinkedList/index.js
@@ -9,16 +9,23 @@ class LinkedList {
   constructor() {
     this.first = null;
     this.last = null;
+    this._size = 0;
   }
 
   addFirst(value) {
     const node = new Node(value);
     if (!this.first) {
-      this.first = this.last = value;
+      this.first = this.last = node;
+    } else {
+      const second = this.first;
+      this.first = node;
+      node.next = second;
     }
-    const second = this.first;
-    this.first = node;
-    node.next = second;
+    this._size++;
+  }
+
+  size() {
+    return this._size;
   }
 }
 
