b2b7f9f474229f5e3b1b09a02f243df81689aea8
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-03-27 19:10:20 +0430

    chore: implement add and remove method of Node

--- algoCamp/tree/index.js
+++ algoCamp/tree/index.js
@@ -3,6 +3,14 @@ class Node {
     this.value = value;
     this.children = [];
   }
+
+  add(value) {
+    this.children.push(new Node(value));
+  }
+
+  remove(value) {
+    this.children = this.children.filter(node.value !== value);
+  }
 }
 
 class Tree {}
--- algoCamp/tree/index.test.js
+++ algoCamp/tree/index.test.js
@@ -11,3 +11,11 @@ test('Node should has children and value properties', () => {
   expect(a.value).toEqual('a');
   expect(a.children.length).toEqual(0);
 });
+
+test('Node.add() should add a children to node', () => {
+  const node = new Node('parent');
+  node.add('child one');
+  expect(node.children.length).toEqual(1);
+  expect(node.children[0].value).toEqual('child one');
+  expect(node.children[0].children).toEqual([]);
+});
